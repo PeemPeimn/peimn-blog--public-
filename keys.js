30cfa51f5b676b983b65a2a2cf721cc77b92288e
Peem <intamoon.peem@gmail.com>
2021-11-26 01:34:24 +0700

    Ignore Private Data

--- keys.js
+++
@@ -1,3 +0,0 @@
-export const ENDPOINT = "https://pheamwaruch.documents.azure.com:443/"
-export const WRITE_KEY = "J7Jbl7iUxssa0ltimVpf6GnnMwyuYbo4NsTn4Cx6Q3q3oJsP8mU7T99l6MiLcgOhPomEtC14Ts1vwFmdj1dTDA=="
-export const READ_KEY = "h2LTbxJDWmjYRVHAjx1KBIWwwZLOvuv3gPsBGJ6BCluXBLtsMqg6STjjlkmeBK0kigismSjPbdBsiGRQqKuzCg=="
